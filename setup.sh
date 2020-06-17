8f908d9cb55dd90c75c8bcdce8bf4d1fef543500
James Sweet <james.sweet88@googlemail.com>
2020-06-17 13:27:19 -0400

    Added some tests for git and docker-compose in the setup file

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if ! [ -x "$(command -v git)" ]; then
+  echo 'Error: git is not installed.' >&2
+  exit 1
+fi
+
 if [ ! -d "../trifid" ]; then
   git clone https://github.com/landrs-toolkit/landrs-trifid.git ../trifid
 fi
@@ -10,6 +15,11 @@ fi
 
 if [ ! -d "../demo" ]; then
   git clone https://github.com/landrs-toolkit/landrs-demo-portal.git ../demo
+fi
+
+if ! [ -x "$(command -v docker-compose)" ]; then
+  echo 'Error: docker-compose is not installed.' >&2
+  exit 1
 fi
 
 docker-compose build
